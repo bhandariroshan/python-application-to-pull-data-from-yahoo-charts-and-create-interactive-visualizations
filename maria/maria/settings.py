3ffb7e0a68bd8970fa7de88a4a6a1cf53890f9c7
Roshan Bhandari <brishi98@gmail.com>
2017-02-15 12:30:20 +0545

    changes

--- maria/maria/settings.py
+++ maria/maria/settings.py
@@ -82,7 +82,7 @@ TEMPLATES = [
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
-                'django.core.context_processors.request',
+                # 'django.core.context_processors.request',
                 # Your stuff: custom template context processors go here
             ],
         },
